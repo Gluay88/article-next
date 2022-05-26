04a018dcae0f3e540d85d5930df0489b8cbd7a9a
gluay88 <p.wantagaew@gmail.comm>
2022-05-25 19:31:52 -0700

    added site og

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://article-news-next.herokuapp.com";
+export const server = dev ? "http://localhost:3000" : "http://gluay.com";
